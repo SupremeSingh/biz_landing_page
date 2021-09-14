fcbd006853a2859a514179905ec05ea5d23823dd
money_singh <ms785@duke.edu>
2021-09-13 22:27:51 -0400

    The basic wireframe

--- landing-page/src/App.js
+++ landing-page/src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
 import Navbar from "./component/Navbar";
+import Header from "./component/Header";
+import Feature from "./component/Feature";
+import Steps from "./component/Steps";
+import Signup from "./component/Signup";
+import Footer from "./component/Footer";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Header />
+      <Feature />
+      <Feature />
+      <Feature />
+      <Steps />
+      <Signup />
+      <Footer />
     </div>
   );
 }
