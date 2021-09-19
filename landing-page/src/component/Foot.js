a4b4d184db6047172bffcdf5c8764c7561bd2c02
Franklin Boampong <fboamps@gmail.com>
2021-09-18 21:11:56 -0400

    merge with main

--- landing-page/src/component/Foot.js
+++ landing-page/src/component/Foot.js
@@ -6,12 +6,6 @@ const Foot = () => {
         <div class="lg:flex">
             <div class="w-full -mx-6 lg:w-2/5">
                 <div class="px-6">
-                    <div>
-                        <a href="#" class="text-xl font-bold text-white dark:text-white hover:text-gray-700 dark:hover:text-gray-300">Brand</a>
-                    </div>
-                    
-                    <p class="max-w-md mt-2 text-white dark:text-gray-400">Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis, nisi! Id.</p>
-                    
                     <div class="flex mt-4 -mx-2">
                         <a href="#" class="mx-2 text-white dark:text-gray-200 hover:text-gray-600 dark:hover:text-gray-400" aria-label="Linkden">
                             <svg class="w-4 h-4 fill-current" viewBox="0 0 512 512">
@@ -35,32 +29,17 @@ const Foot = () => {
             </div>
 
             <div class="mt-6 lg:mt-0 lg:flex-1">
-                <div class="grid grid-cols-2 gap-6 sm:grid-cols-3 md:grid-cols-4">
+                <div class="grid center grid-cols-2 gap-4 sm:grid-cols-3 md:grid-cols-4">
                     <div>
-                        <h3 class="text-white uppercase dark:text-white">About</h3>
-                        <a href="#" class="block mt-2 text-sm text-white dark:text-gray-400 hover:underline">Company</a>
-                        <a href="#" class="block mt-2 text-sm text-white dark:text-gray-400 hover:underline">community</a>
-                        <a href="#" class="block mt-2 text-sm text-white dark:text-gray-400 hover:underline">Careers</a>
+                        <h3 class="text-white dark:text-white">Blog</h3>
                     </div>
 
                     <div>
-                        <h3 class="text-white uppercase dark:text-white">Blog</h3>
-                        <a href="#" class="block mt-2 text-sm text-white dark:text-gray-400 hover:underline">Tec</a>
-                        <a href="#" class="block mt-2 text-sm text-white dark:text-gray-400 hover:underline">Music</a>
-                        <a href="#" class="block mt-2 text-sm text-white dark:text-gray-400 hover:underline">Videos</a>
+                        <h3 class="text-white dark:text-white">Team</h3>
                     </div>
 
                     <div>
-                        <h3 class="text-white uppercase dark:text-white">Products</h3>
-                        <a href="#" class="block mt-2 text-sm text-white dark:text-gray-400 hover:underline">Mega cloud</a>
-                        <a href="#" class="block mt-2 text-sm text-white dark:text-gray-400 hover:underline">Aperion UI</a>
-                        <a href="#" class="block mt-2 text-sm text-white dark:text-gray-400 hover:underline">Meraki UI</a>
-                    </div>
-
-                    <div>
-                        <h3 class="text-white uppercase dark:text-white">Contact</h3>
-                        <span class="block mt-2 text-sm text-white dark:text-gray-400 hover:underline">+1 526 654 8965</span>
-                        <span class="block mt-2 text-sm text-white dark:text-gray-400 hover:underline">example@email.com</span>
+                        <h3 class="text-white dark:text-white">Contact</h3>
                     </div>
                 </div>
             </div>
--- landing-page/src/component/Navbar.js
+++ landing-page/src/component/Navbar.js
@@ -74,8 +74,8 @@ const Navbar = () => {
           </li>
           <li>
             <a
-              href="/"
-              class="inline-flex items-center justify-center h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
+              href="/registration"
+              class="inline-flex items-center justify-center h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-blue-600 hover:bg-blue-=400 focus:shadow-outline focus:outline-none"
               aria-label="Sign up"
               title="Sign up"
             >
